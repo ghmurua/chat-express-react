883d05a21423b08f9aab4d3f5af84c92266dbffc
Gabriel Murua <gmuruadev@gmail.com>
2022-09-09 17:06:08 -0300

    Commit inicial

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from 'cors'
+import { PORT } from "./config.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+app.use(cors())
+app.use(morgan("dev"));
+
+app.listen(PORT, () => console.log("server on port", PORT));
