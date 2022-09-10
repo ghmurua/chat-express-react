772e930cda4e93c54ee1bc4e4f6318f42c21aa32
Gabriel Murua <gmuruadev@gmail.com>
2022-09-09 21:41:17 -0300

    manejando mensajes entre cliente servidor con socket.io-client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,66 @@
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io("http://localhost:4000");
+
 export default function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      body: "Welcome!",
+      from: "system",
+    },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(message);
+    socket.emit("message", message);
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+    setMessage("");
+  };
+
+  useEffect(() => {
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+      console.log(message);
+    };
+
+    socket.on("message", receiveMessage);
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, [messages]);
+
   return (
-    <div className="App">
-      <h1>Chat App</h1>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1>Chat App</h1>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          className="border-2 border-zinc-500 p-2 text-black w-full"
+        />
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li key={index} 
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" 
+                ? "bg-sky-700 ml-auto" 
+                : "bg-black"}`}>
+              <p>
+                {message.from}: {message.body}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
-  )
+  );
 }
--- server/index.js
+++ server/index.js
@@ -2,14 +2,31 @@ import express from "express";
 import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
 import http from "http";
-import cors from 'cors'
+import cors from "cors";
 import { PORT } from "./config.js";
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  cors: {
+    // origin: 'http://localhost:5173'
+    origin: "*",
+  },
+});
 
-app.use(cors())
+app.use(cors());
 app.use(morgan("dev"));
 
-app.listen(PORT, () => console.log("server on port", PORT));
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on("message", (message) => {
+    // console.log(socket.id, message);
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id.slice(0,5)
+    });
+  });
+});
+
+server.listen(PORT, () => console.log("server on port", PORT));
